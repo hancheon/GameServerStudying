3bf1301e504eaa09709eb2571ec2d9d45691a87f
Yun JuYeon <a01071809269@gmail.com>
2025-02-19 11:51:47 +0900

    Feat: Select 모델 서버 추가

---
+++ Socket/SelectTcpServer/SelectTcpServer/SelectTcpServer.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("%d\n", WSAGetLastError());
+        return -1;
+    }
+
+
+}
