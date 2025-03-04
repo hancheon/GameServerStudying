201a5aeef287ca881854f30644b2ca84b05628b2
Yun JuYeon <a01071809269@gmail.com>
2025-03-04 13:02:21 +0900

    Delete: 링버퍼 서버 클라 대신 라이브러리로 교체

--- Socket/RingBuffer/RingBuffer_Client/RingBuffer_Client/RingBuffer_Client.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-#include <winsock.h>
-#include <WS2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
-
-int main()
-{
-    
-}
--- Socket/RingBuffer/RingBuffer_Server/RingBuffer_Server/Console.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define SCREEN_HEIGHT 24
-#define SCREEN_WIDTH 81
-
-// 함수 선언
-void consoleInit(void);
-void moveCursor(int, int);
