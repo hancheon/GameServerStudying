56bc52135820c96c17959594d405ad3bbe1cc496
Yun JuYeon <a01071809269@gmail.com>
2025-03-17 14:34:46 +0900

    Init: 직렬화된 버퍼 라이브러리 생성

---
+++ MyLibrary/SerializeBuffer/SerializeBuffer/Packet.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include "Packet.h"
+
+Packet::Packet() : m_bufferSize(DEFUALT_BUFSIZE), m_dataSize(0), m_front(-1), m_rear(-1)
+{
+	m_bufferPtr = new char[m_bufferSize];
+}
+
+Packet::Packet(int size) : m_bufferSize(size), m_dataSize(0), m_front(-1), m_rear(-1)
+{
+	m_bufferPtr = new char[m_bufferSize];
+}
+
+Packet::~Packet()
+{
+	delete[] m_bufferPtr;
+}
+
+void Packet::Clear()
+{
+	m_front = m_rear = -1;
+	m_dataSize = 0;
+}
+
+int Packet::GetBufferSize()
+{
+	return m_bufferSize;
+}
+
+int Packet::GetDataSize()
+{
+	return m_dataSize;
+}
+
+char* Packet::GetBufferPtr()
+{
+	return m_bufferPtr;
+}
+
+int Packet::PutData(char* srcData, int size)
+{
+	memcpy(m_bufferPtr + m_rear + 1, srcData, size);
+	m_rear = (m_rear + size) % m_bufferSize;
+	m_dataSize += size;
+	return size;
+}
+
+int Packet::GetData(char* desData, int size)
+{
+	memcpy(desData, m_bufferPtr + m_front + 1, size);
+	m_front = (m_front + size) % m_bufferSize;
+	m_dataSize -= size;
+	return size;
+}
+
+int Packet::MoveWritePos(unsigned int size)
+{
+	m_rear = (m_rear + size) % m_bufferSize;
+	m_dataSize += size;
+	return size;
+}
+
+int Packet::MoveReadPos(unsigned int size)
+{
+	m_front = (m_front + size) % m_bufferSize;
+	m_dataSize -= size;
+	return size;
+}
+
+Packet& Packet::operator=(Packet& srcPacket)
+{
+	memcpy(m_bufferPtr, &srcPacket, srcPacket.GetDataSize());
+	return *this;
+}
+
+Packet& Packet::operator<<(unsigned char value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(unsigned char));
+	m_rear = (m_rear + sizeof(unsigned char)) % m_bufferSize;
+	m_dataSize += sizeof(unsigned char);
+	return *this;
+}
+
+Packet& Packet::operator<<(char value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(char));
+	m_rear = (m_rear + sizeof(char)) % m_bufferSize;
+	m_dataSize += sizeof(char);
+	return *this;
+}
+
+Packet& Packet::operator<<(unsigned short value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(unsigned short));
+	m_rear = (m_rear + sizeof(unsigned short)) % m_bufferSize;
+	m_dataSize += sizeof(unsigned short);
+	return *this;
+}
+
+Packet& Packet::operator<<(short value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(short));
+	m_rear = (m_rear + sizeof(short)) % m_bufferSize;
+	m_dataSize += sizeof(short);
+	return *this;
+}
+
+Packet& Packet::operator<<(int value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(int));
+	m_rear = (m_rear + sizeof(int)) % m_bufferSize;
+	m_dataSize += sizeof(int);
+	return *this;
+}
+
+Packet& Packet::operator<<(long value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(long));
+	m_rear = (m_rear + sizeof(long)) % m_bufferSize;
+	m_dataSize += sizeof(long);
+	return *this;
+}
+
+Packet& Packet::operator<<(float value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(float));
+	m_rear = (m_rear + sizeof(float)) % m_bufferSize;
+	m_dataSize += sizeof(float);
+	return *this;
+}
+
+Packet& Packet::operator<<(__int64 value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(__int64));
+	m_rear = (m_rear + sizeof(__int64)) % m_bufferSize;
+	m_dataSize += sizeof(__int64);
+	return *this;
+}
+
+Packet& Packet::operator<<(double value)
+{
+	memcpy(m_bufferPtr + m_rear + 1, &value, sizeof(double));
+	m_rear = (m_rear + sizeof(double)) % m_bufferSize;
+	m_dataSize += sizeof(double);
+	return *this;
+}
+
+Packet& Packet::operator>>(BYTE& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(BYTE));
+	m_front = (m_front + sizeof(BYTE)) % m_bufferSize;
+	m_dataSize -= sizeof(BYTE);
+	return *this;
+}
+
+Packet& Packet::operator>>(char& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(char));
+	m_front = (m_front + sizeof(char)) % m_bufferSize;
+	m_dataSize -= sizeof(char);
+	return *this;
+}
+
+Packet& Packet::operator>>(WORD& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(WORD));
+	m_front = (m_front + sizeof(WORD)) % m_bufferSize;
+	m_dataSize -= sizeof(WORD);
+	return *this;
+}
+
+Packet& Packet::operator>>(short& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(short));
+	m_front = (m_front + sizeof(short)) % m_bufferSize;
+	m_dataSize -= sizeof(short);
+	return *this;
+}
+
+Packet& Packet::operator>>(int& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(int));
+	m_front = (m_front + sizeof(int)) % m_bufferSize;
+	m_dataSize -= sizeof(int);
+	return *this;
+}
+
+Packet& Packet::operator>>(DWORD& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(DWORD));
+	m_front = (m_front + sizeof(DWORD)) % m_bufferSize;
+	m_dataSize -= sizeof(DWORD);
+	return *this;
+}
+
+Packet& Packet::operator>>(float& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(float));
+	m_front = (m_front + sizeof(float)) % m_bufferSize;
+	m_dataSize -= sizeof(float);
+	return *this;
+}
+
+Packet& Packet::operator>>(__int64& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(__int64));
+	m_front = (m_front + sizeof(__int64)) % m_bufferSize;
+	m_dataSize -= sizeof(__int64);
+	return *this;
+}
+
+Packet& Packet::operator>>(double& value)
+{
+	memcpy(&value, m_bufferPtr + m_front + 1, sizeof(double));
+	m_front = (m_front + sizeof(double)) % m_bufferSize;
+	m_dataSize -= sizeof(double);
+	return *this;
+}
---
+++ MyLibrary/SerializeBuffer/SerializeBuffer/Packet.h
@@ -0,0 +1,69 @@
+#ifndef __PACKET__
+#define __PACKET__
+
+#include <Windows.h>
+#define DEFUALT_BUFSIZE 1000
+
+class Packet
+{
+public:
+	Packet();
+	Packet(int);
+	virtual ~Packet();
+
+	void Clear();
+
+	int GetBufferSize();
+	int GetDataSize();
+
+	// 버퍼 시작 주소 포인터 반환
+	char* GetBufferPtr();
+
+	// 데이터 직접 삽입 및 추출
+	int GetData(char*, int);
+	int PutData(char*, int);
+
+	// 이동한 사이즈 반환
+	int MoveWritePos(unsigned int);
+	int MoveReadPos(unsigned int);
+
+	// 넣기 연산자 오버로딩
+	Packet& operator=(Packet&);
+
+	Packet& operator<<(unsigned char);
+	Packet& operator<<(char);
+
+	Packet& operator<<(unsigned short);
+	Packet& operator<<(short);
+
+	Packet& operator<<(int);
+	Packet& operator<<(long);
+	Packet& operator<<(float);
+
+	Packet& operator<<(__int64);
+	Packet& operator<<(double);
+
+	// 빼기 연산자 오버로딩
+	Packet& operator>>(BYTE&);
+	Packet& operator>>(char&);
+
+	Packet& operator>>(WORD&);
+	Packet& operator>>(short&);
+
+	Packet& operator>>(int&);
+	Packet& operator>>(DWORD&);
+	Packet& operator>>(float&);
+
+	Packet& operator>>(__int64&);
+	Packet& operator>>(double&);
+
+private:
+	char* m_bufferPtr;
+	int m_bufferSize;
+	int m_dataSize;
+
+	int m_front;
+	int m_rear;
+};
+
+#endif
