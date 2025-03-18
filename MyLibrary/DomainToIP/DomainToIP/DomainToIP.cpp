421f5b18fa45b0c7f7f45d04a48ccb86b1df8d1f
Yun JuYeon <a01071809269@gmail.com>
2025-03-19 00:51:39 +0900

    Init: 도메인->ip(10진수) 변환 라이브러리

---
+++ MyLibrary/DomainToIP/DomainToIP/DomainToIP.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+
+BOOL DomainToIP(wchar_t* domain, IN_ADDR* addr)
+{
+    ADDRINFOW* addrInfo;
+    SOCKADDR_IN* sockAddr;
+
+    if (GetAddrInfo(domain, L"0", NULL, &addrInfo) != 0)
+    {
+        return FALSE;
+    }
+
+    sockAddr = (SOCKADDR_IN*)addrInfo->ai_addr;
+    *addr = sockAddr->sin_addr;
+    FreeAddrInfo(addrInfo);
+    return TRUE;
+}
