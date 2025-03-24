cc2202f80aec7e2bbe62ddb9f85c740469646933
Yun JuYeon <a01071809269@gmail.com>
2025-03-24 13:56:55 +0900

    Fix: IDLC 패킷 생성부 프록시 스텁 나누기

--- Socket/TCPFighter_MessageCompiler/TCPFighter_MessageCompiler/TCPFighter_MessageCompiler.cpp
+++ Socket/TCPFighter_MessageCompiler/TCPFighter_MessageCompiler/TCPFighter_MessageCompiler.cpp
@@ -1,73 +1,87 @@
 ﻿#include <iostream>
+#include <string>
 
 char* IDLfileName;
 int fileNameSize;
 char* messageBuffer;
 int fileSize;
 
-void makeHeaderFile();
-void makeSourceFile();
+void makeProxyHeader();
+void makeProxySource();
 
 int main(int argc, char* argv[])
 {
-    FILE* message;
-    fopen_s(&message, argv[1], "rb");
-    if (message == nullptr)
-        return -1;
+	FILE* message;
+	fopen_s(&message, argv[1], "rb");
+	if (message == nullptr)
+		return -1;
 
-    fseek(message, 0, SEEK_END);
-    fileSize = ftell(message);
-    fseek(message, 0, SEEK_SET);
+	fseek(message, 0, SEEK_END);
+	fileSize = ftell(message);
+	fseek(message, 0, SEEK_SET);
 
-    messageBuffer = new char[fileSize];
-    fread(messageBuffer, fileSize, 1, message);
+	messageBuffer = new char[fileSize];
+	fread(messageBuffer, fileSize, 1, message);
 
-    char* context = nullptr;
-    IDLfileName = strtok_s(argv[1], ".", &context);
-    fileNameSize = strlen(IDLfileName);
+	char* context = nullptr;
+	IDLfileName = strtok_s(argv[1], ".", &context);
+	fileNameSize = strlen(IDLfileName);
 
-    makeHeaderFile();
-    makeSourceFile();
+	makeProxyHeader();
+	makeProxySource();
 }
 
-void makeHeaderFile()
+void makeProxyHeader()
 {
-    FILE* headerFile;
-    char* fileName = new char[fileNameSize + 3];
-    strcpy_s(fileName, fileNameSize + 3, IDLfileName);
-    strcat_s(fileName, fileNameSize + 3, ".h");
-    fopen_s(&headerFile, fileName, "wb");
-    if (headerFile == nullptr)
-        return;
+	FILE* headerFile;
+	const char* fileType = "Proxy.h";
+	int len = fileNameSize + strlen(fileType) + 1;
+	char* fileName = new char[len];
+	strcpy_s(fileName, len, IDLfileName);
+	strcat_s(fileName, len, fileType);
+	fopen_s(&headerFile, fileName, "wb");
+	if (headerFile == nullptr)
+		return;
 
-    const char* includes = "#include <Windows.h>\r\n#include \"Packet.h\"\r\n\r\n";
-    fwrite(includes, strlen(includes), 1, headerFile);
- 
-    int pType;
-    char* func;
-    char* context = nullptr;
-    while (1)
-    {
-        pType = atoi(strtok_s(messageBuffer, " : ", &context));
-        func = strtok_s(nullptr, "\r\n", &context);
-    }
+	const char* includes = "#pragma once\r\n#include <Windows.h>\r\n#include \"Packet.h\"\r\n\r\n";
+	fwrite(includes, strlen(includes), 1, headerFile);
 
-    fwrite(messageBuffer, fileSize, 1, headerFile);
+	char* context = nullptr;
+	strtok_s(messageBuffer, "}", &context);
+	while (1)
+	{
+		int pType;
+		const char* funcType = "void ";
+		char* func;
 
-    delete[] fileName;
+		pType = atoi(strtok_s(messageBuffer, "  ", &context));
+		strtok_s(nullptr, " ", &context);
+		func = strtok_s(nullptr, "\r\n", &context);
+		len = strlen(func) + strlen(funcType) + 1;
+		char* funcDeclaration = new char[len];
+		strcpy_s(funcDeclaration, len, funcType);
+		strcat_s(funcDeclaration, len, func);
+		fwrite(funcDeclaration, strlen(funcDeclaration), 1, headerFile);
+
+		delete[] funcDeclaration;
+	}
+
+	delete[] fileName;
 }
 
-void makeSourceFile()
+void makeProxySource()
 {
-    FILE* sourceFile;
-    char* fileName = new char[fileNameSize + 5];
-    strcpy_s(fileName, fileNameSize + 5, IDLfileName);
-    strcat_s(fileName, fileNameSize + 5, ".cpp");
-    fopen_s(&sourceFile, fileName, "wb");
-    if (sourceFile == nullptr)
-        return;
+	FILE* sourceFile;
+	const char* fileType = "Proxy.cpp";
+	int len = fileNameSize + strlen(fileType) + 1;
+	char* fileName = new char[len];
+	strcpy_s(fileName, len, IDLfileName);
+	strcat_s(fileName, len, fileType);
+	fopen_s(&sourceFile, fileName, "wb");
+	if (sourceFile == nullptr)
+		return;
 
-    fwrite(messageBuffer, fileSize, 1, sourceFile);
+	fwrite(messageBuffer, fileSize, 1, sourceFile);
 
-    delete[] fileName;
+	delete[] fileName;
 }
