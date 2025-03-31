3e832d6d3100c36bf128595c8dbf627715db3eca
Yun JuYeon <a01071809269@gmail.com>
2025-03-31 15:09:46 +0900

    Feat: 출발점 도착점 기능 추가 및 간단한 뼈대 추가

--- AStar/AStar/AStar.cpp
+++ AStar/AStar/AStar.cpp
@@ -4,6 +4,8 @@
 #include "framework.h"
 #include "AStar.h"
 #include "windowsx.h"
+#include <iostream>
+#include <list>
 
 #define MAX_LOADSTRING 100
 
@@ -11,14 +13,46 @@
 #define GRID_WIDTH 100
 #define GRID_HEIGHT 50
 
+int increaseX[8] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int increaseY[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
+
+enum BLOCK_COLOR
+{
+    OBSTACLE = 1, BEGIN, END, OPEN, CLOSE, PATH
+};
+
+struct Node
+{
+    int x;
+    int y;
+    int g; // 출발지에서 노드까지 비용 -> 맨해튼
+    double h; // 노드에서 목적지까지 비용 -> 유클리드
+    int f; // 출발지에서 해당 노드 거쳐 목적지까지 비용
+};
+
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
-HBRUSH hTileBrush;
+HBITMAP hMemDCBitmap;
+HBITMAP hOldMemDCBitmap;
+HDC hMemDC;
+RECT memDCRect;
+
+HBRUSH hObstacleBrush;
+HBRUSH hBeginBrush;
+HBRUSH hEndBrush;
+HBRUSH hCloseBrush;
+HBRUSH hOpenBrush;
 HPEN hGridPen;
 char tile[GRID_HEIGHT][GRID_WIDTH]; // 0: 장애물 없음, 1: 장애물 있음
+
+Node* beginNode;
+Node* endNode;
+
+//int beginToNodeMode = MANHATTAN;
+//int nodeToEndMode = EUCLID;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -77,7 +111,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
     wcex.lpfnWndProc    = WndProc;
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
@@ -106,7 +140,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   HWND hWnd = CreateWindowW(szWindowClass, L"Path Finding", WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
@@ -120,9 +154,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-bool isErase = false;
-bool isDrag = false;
-
+//===================================================================//
+// 그리드 출력
+//===================================================================//
 void RenderGrid(HDC hdc)
 {
     int x = 0;
@@ -144,23 +178,54 @@ void RenderGrid(HDC hdc)
     SelectObject(hdc, hOldPen);
 }
 
-void RenderObstacle(HDC hdc)
+//===================================================================//
+// 칸 색칠
+// 0: 빈칸, 1: 장애물, 2: 시작점, 3: 도착점
+// 4: OpenList, 5: CloseList, 6: 탐색된 길
+//===================================================================//
+void RenderBlock(HDC hdc)
 {
     int x = 0;
     int y = 0;
-    HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hTileBrush);
-    SelectObject(hdc, GetStockObject(NULL_PEN));
+
+    HBRUSH hOldBrush = NULL;
 
     for (int wCnt = 0; wCnt < GRID_WIDTH; wCnt++)
     {
         for (int hCnt = 0; hCnt < GRID_HEIGHT; hCnt++)
         {
-            if (tile[hCnt][wCnt] == 1)
+            switch (tile[hCnt][wCnt])
             {
+            case 1:
+            {
+                hOldBrush = (HBRUSH)SelectObject(hdc, hObstacleBrush);
+                SelectObject(hdc, GetStockObject(NULL_PEN));
                 x = wCnt * GRID_SIZE;
                 y = hCnt * GRID_SIZE;
-
                 Rectangle(hdc, x, y, x + GRID_SIZE + 2, y + GRID_SIZE + 2);
+            }
+                break;
+            case 2:
+            {
+                hOldBrush = (HBRUSH)SelectObject(hdc, hBeginBrush);
+                SelectObject(hdc, GetStockObject(NULL_PEN));
+                x = wCnt * GRID_SIZE;
+                y = hCnt * GRID_SIZE;
+                Rectangle(hdc, x, y, x + GRID_SIZE + 2, y + GRID_SIZE + 2);
+            }
+                break;
+            case 3:
+            {
+                hOldBrush = (HBRUSH)SelectObject(hdc, hEndBrush);
+                SelectObject(hdc, GetStockObject(NULL_PEN));
+                x = wCnt * GRID_SIZE;
+                y = hCnt * GRID_SIZE;
+                Rectangle(hdc, x, y, x + GRID_SIZE + 2, y + GRID_SIZE + 2);
+            }
+                break;
+            default:
+                hOldBrush = (HBRUSH)SelectObject(hdc, hObstacleBrush);
+                break;
             }
         }
     }
@@ -168,52 +233,73 @@ void RenderObstacle(HDC hdc)
     SelectObject(hdc, hOldBrush);
 }
 
-void RenderStart(HDC hdc)
+//===================================================================//
+// A* 길찾기 로직
+//===================================================================//
+int BeginToNode(Node* node)
 {
-    int x = 0;
-    int y = 0;
-    HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, CreateSolidBrush(RGB(100, 0, 100)));
-    SelectObject(hdc, GetStockObject(NULL_PEN));
-
-    for (int wCnt = 0; wCnt < GRID_WIDTH; wCnt++)
-    {
-        for (int hCnt = 0; hCnt < GRID_HEIGHT; hCnt++)
-        {
-            if (tile[hCnt][wCnt] == 2)
-            {
-                x = wCnt * GRID_SIZE;
-                y = hCnt * GRID_SIZE;
-
-                Rectangle(hdc, x, y, x + GRID_SIZE + 2, y + GRID_SIZE + 2);
-            }
-        }
-    }
-
-    SelectObject(hdc, hOldBrush);
+    node->g = abs(node->x - beginNode->x) + abs(node->y - beginNode->y);
 }
 
-void RenderEnd(HDC hdc)
+double NodeToEnd(Node* node)
 {
-    int x = 0;
-    int y = 0;
-    HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, CreateSolidBrush(RGB(0, 100, 0)));
-    SelectObject(hdc, GetStockObject(NULL_PEN));
+    node->h = sqrt(pow(endNode->x - node->x, 2) + pow(endNode->y - node->y, 2));
+}
 
-    for (int wCnt = 0; wCnt < GRID_WIDTH; wCnt++)
+double Matrix(Node* node)
+{
+    node->f = node->g + node->h;
+}
+
+bool isArrive(Node* node)
+{
+    return (node->x == endNode->x) && (node->y == endNode->y);
+}
+
+void FindPath()
+{
+    if (beginNode == NULL || endNode == NULL)
+        return;
+
+    std::list<Node*> OpenList;
+    std::list<Node*> CloseList;
+    OpenList.push_back(beginNode);
+    while (OpenList.size() != 0)
     {
-        for (int hCnt = 0; hCnt < GRID_HEIGHT; hCnt++)
+        Node* node = OpenList.back();
+        OpenList.pop_back();
+        CloseList.push_back(node);
+        
+        for (int cnt = 0; cnt < 8; cnt++)
         {
-            if (tile[hCnt][wCnt] == 3)
-            {
-                x = wCnt * GRID_SIZE;
-                y = hCnt * GRID_SIZE;
+            Node* nextNode = new Node;
+            nextNode->x = node->x + increaseX[cnt];
+            nextNode->y = node->y + increaseY[cnt];
 
-                Rectangle(hdc, x, y, x + GRID_SIZE + 2, y + GRID_SIZE + 2);
+            // 유효한 좌표인지 확인
+            if (nextNode->x < 0 || nextNode->x > GRID_WIDTH || nextNode->y < 0 || nextNode->y > GRID_HEIGHT)
+            {
+                delete nextNode;
+                continue;
             }
+
+            // 목적지인지 확인
+            if (isArrive(nextNode))
+                return;
+
+            // CloseList에 있는지 확인
+            std::list<Node*>::iterator iter;
+            for (iter = CloseList.begin(); iter != CloseList.end(); iter++)
+            {
+                if ((*iter)->x == nextNode->x && (*iter)->y == nextNode->y)
+                {
+                    break;
+                }
+            }
+
+            OpenList.push_back(nextNode);
         }
     }
-
-    SelectObject(hdc, hOldBrush);
 }
 
 //
@@ -226,41 +312,117 @@ void RenderEnd(HDC hdc)
 //  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 //  WM_LBUTTONDBLCLK    - 출발점 설정
 //  WM_RBUTTONDBLCLK    - 도착점 설정
-//  WM_LBUTTONDOWN  - 장애물 등록: 0
-//  WM_RBUTTONDOWN  - 장애물 취소: 1
+//  WM_LBUTTONDOWN  - 장애물 등록
+//  WM_RBUTTONDOWN  - 장애물 취소
 //
+
+bool isErase = false;
+bool isDrag = false;
+bool isBegin = false;
+bool isEnd = false;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT ps;
     HDC hdc;
+    PAINTSTRUCT ps;
 
     switch (message)
     {
     case WM_LBUTTONDBLCLK: // 출발지 설정
     {
-        int xPos = GET_X_LPARAM(lParam);
-        int yPos = GET_Y_LPARAM(lParam);
-        int tileX = xPos / GRID_SIZE;
-        int tileY = yPos / GRID_SIZE;
-
-        if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+        if (!isBegin)
         {
-            tile[tileY][tileX] = 2;
-            InvalidateRect(hWnd, NULL, true);
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+            int tileX = xPos / GRID_SIZE;
+            int tileY = yPos / GRID_SIZE;
+
+            if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+            {
+                tile[tileY][tileX] = BEGIN;
+                InvalidateRect(hWnd, NULL, false);
+            }
+
+            beginNode = new Node;
+            beginNode->x = tileX;
+            beginNode->y = tileY;
+
+            isBegin = true;
+        }
+        else
+        {
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+            int tileX = xPos / GRID_SIZE;
+            int tileY = yPos / GRID_SIZE;
+
+            if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+            {
+                tile[beginNode->y][beginNode->x] = 0;
+                if (beginNode->x == tileX && beginNode->y == tileY)
+                {
+                    delete beginNode;
+                    isBegin = false;
+                    InvalidateRect(hWnd, NULL, false);
+                    break;
+                }
+                tile[tileY][tileX] = BEGIN;
+                InvalidateRect(hWnd, NULL, false);
+            }
+
+            beginNode->x = tileX;
+            beginNode->y = tileY;
+
+            isBegin = true;
         }
     }
     break;
     case WM_RBUTTONDBLCLK: // 도착지 설정
     {
-        int xPos = GET_X_LPARAM(lParam);
-        int yPos = GET_Y_LPARAM(lParam);
-        int tileX = xPos / GRID_SIZE;
-        int tileY = yPos / GRID_SIZE;
-
-        if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+        if (!isEnd)
         {
-            tile[tileY][tileX] = 3;
-            InvalidateRect(hWnd, NULL, true);
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+            int tileX = xPos / GRID_SIZE;
+            int tileY = yPos / GRID_SIZE;
+
+            if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+            {
+                tile[tileY][tileX] = END;
+                InvalidateRect(hWnd, NULL, false);
+            }
+
+            endNode = new Node;
+            endNode->x = tileX;
+            endNode->y = tileY;
+
+            isEnd = true;
+        }
+        else
+        {
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+            int tileX = xPos / GRID_SIZE;
+            int tileY = yPos / GRID_SIZE;
+
+            if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
+            {
+                tile[endNode->y][endNode->x] = 0;
+                if (endNode->x == tileX && endNode->y == tileY)
+                {
+                    delete endNode;
+                    isEnd = false;
+                    InvalidateRect(hWnd, NULL, false);
+                    break;
+                }
+                tile[tileY][tileX] = END;
+                InvalidateRect(hWnd, NULL, false);
+            }
+
+            endNode->x = tileX;
+            endNode->y = tileY;
+
+            isEnd = true;
         }
     }
     break;
@@ -290,48 +452,80 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             if (tileX >= 0 && tileX < GRID_WIDTH && tileY >= 0 && tileY < GRID_HEIGHT)
             {
                 tile[tileY][tileX] = (!isErase);
-                InvalidateRect(hWnd, NULL, true);
+                InvalidateRect(hWnd, NULL, false);
             }
         }
     }
     break;
+    case WM_KEYDOWN:
+    {
+        if (wParam == VK_DELETE)
+        {
+            memset(tile, 0, sizeof(tile));
+            InvalidateRect(hWnd, NULL, false);
+        }
+        else if (wParam == VK_RETURN)
+        {
+            FindPath();
+        }
+    }
     case WM_CREATE:
+    {
+        hdc = GetDC(hWnd);
+        GetClientRect(hWnd, &memDCRect);
+        hMemDCBitmap = CreateCompatibleBitmap(hdc, memDCRect.right, memDCRect.bottom);
+        hMemDC = CreateCompatibleDC(hdc);
+        ReleaseDC(hWnd, hdc);
+        hOldMemDCBitmap = (HBITMAP)SelectObject(hMemDC, hMemDCBitmap);
         hGridPen = CreatePen(PS_SOLID, 1, RGB(200, 200, 200));
-        hTileBrush = CreateSolidBrush(RGB(100, 100, 100));
+        hObstacleBrush = CreateSolidBrush(RGB(100, 100, 100));
+        hBeginBrush = CreateSolidBrush(RGB(0, 200, 0));
+        hEndBrush = CreateSolidBrush(RGB(200, 0, 0));
+        hCloseBrush = CreateSolidBrush(RGB(200, 0, 200));
+        hOpenBrush = CreateSolidBrush(RGB(0, 200, 200));
         break;
+    }
     case WM_PAINT:
+    {
+        // 메모리 DC 클리어
+        PatBlt(hMemDC, 0, 0, memDCRect.right, memDCRect.bottom, WHITENESS);
+
+        // 메모리 DC에 출력
+        RenderBlock(hMemDC);
+        RenderGrid(hMemDC);
+
+        // 메모리 DC -> 윈도우 DC Swap
         hdc = BeginPaint(hWnd, &ps);
-        RenderObstacle(hdc);
-        RenderGrid(hdc);
+        BitBlt(hdc, 0, 0, memDCRect.right, memDCRect.bottom, hMemDC, 0, 0, SRCCOPY);
         EndPaint(hWnd, &ps);
         break;
+    }
     case WM_DESTROY:
-        DeleteObject(hTileBrush);
-        DeleteObject(hGridPen);
+        SelectObject(hMemDC, hOldMemDCBitmap);
+        DeleteObject(hMemDC);
+        DeleteObject(hMemDCBitmap);
+        DeleteObject(hObstacleBrush);
         PostQuitMessage(0);
+        break;
+    case WM_SIZE:
+    {
+        SelectObject(hMemDC, hOldMemDCBitmap);
+        DeleteObject(hMemDC);
+        DeleteObject(hMemDCBitmap);
+
+        hdc = GetDC(hWnd);
+
+        GetClientRect(hWnd, &memDCRect);
+        hMemDCBitmap = CreateCompatibleBitmap(hdc, memDCRect.right, memDCRect.bottom);
+        hMemDC = CreateCompatibleDC(hdc);
+        ReleaseDC(hWnd, hdc);
+
+        hOldMemDCBitmap = (HBITMAP)SelectObject(hMemDC, hMemDCBitmap);
+    }
+    case WM_MOUSEWHEEL: // 확대 축소 (각 노드 속성 보기 위해서)
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
